a84d0b637e89edf1fe39e44a618b40a28b31aebe
Jimm <nachosaurus@hotmail.es>
2021-11-11 14:25:05 +0100

    arreglar parte Logica y extraer botones

--- tresenraya/src/main/java/com/example/tresenraya/MainActivity.java
+++ tresenraya/src/main/java/com/example/tresenraya/MainActivity.java
@@ -9,34 +9,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private boolean juegalamaquina;
     private boolean jueganX;
     private boolean[] pulsado;
     private LogicaTresEnRaya partida;
-
+    private Button empezarDeNuevo;
+    private TableRow fila0;
+    private TableRow fila1;
+    private TableRow fila2;
+    private Button boton0;
+    private Button boton1;
+    private Button boton2;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        crearBotonEmpezar();
+        CrearPartida();
+    }
+
+    private void crearBotonEmpezar() {
+        empezarDeNuevo= findViewById(R.id.EmpezarPartida);
+        empezarDeNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               partida.iniciar();
+               iniciarBoleanpulsado();
+            }
+        });
+    }
+
+    private void CrearPartida() {
         CrearTabla();
         iniciarBoleanpulsado();
         partida= new LogicaTresEnRaya();
         juegalamaquina=false;
         jueganX=true;
     }
+
     private void CrearTabla() {
         //int numnames = names.length;
         TableLayout tablero = findViewById(R.id.Tablero);
-        TableRow fila0 = new TableRow(this);
-        TableRow fila1 = new TableRow(this);
-        TableRow fila2 = new TableRow(this);
-        fila0.setLayoutParams(getLayoutParams());
-        fila1.setLayoutParams(getLayoutParams());
-        fila2.setLayoutParams(getLayoutParams());
-        fila0.addView(getButton(0, Color.BLACK, Typeface.BOLD, Color.GREEN));
+        fila0 = new TableRow(this);
+        fila1 = new TableRow(this);
+        fila2 = new TableRow(this);
+        creaBoton0();
+        fila0.addView(boton0);
         fila0.addView(getButton(1, Color.BLACK, Typeface.BOLD, Color.GREEN));
         fila0.addView(getButton(2, Color.BLACK, Typeface.BOLD, Color.GREEN));
         fila1.addView(getButton(3, Color.BLACK, Typeface.BOLD, Color.GREEN));
@@ -58,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         //fijamos el relleno (espacio dentro del View)
         button.setPadding(20, 20, 20, 20);
         //fijamos el tipo de la letra y el estilo
-        button.setTypeface(Typeface.DEFAULT, typeface);
+        button.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
         button.setBackgroundColor(bgColor);
         //suministra parámetros al padre de la View
         button.setLayoutParams(getLayoutParams());
@@ -70,10 +92,20 @@ public class MainActivity extends AppCompatActivity {
                         puntuar(id);
                         button.setText("X");
                         jueganX= false;
+                        mandarMensaje("Juega el Jugador 2");
+                        if(partida.GanaJugador1()==true){
+                            mandarMensaje("Ha ganado el Jugador 1");
+                            bloquearBotones();
+                        }
                     }else{
                         puntuar(id);
                         button.setText("O");
                         jueganX= true;
+                        mandarMensaje("Juega el Jugador 1");
+                        if(partida.GanaJugador2()==true){
+                            mandarMensaje("Ha ganado el Jugador 2");
+                            bloquearBotones();
+                        }
                     }
                     pulsado[id]= true;
                 }
@@ -81,8 +113,48 @@ public class MainActivity extends AppCompatActivity {
         });
         return button;
     }
+    private void creaBoton0() {
+        int id =0;
+         boton0 = new Button(this);
+        boton0.setId(id);
+        boton0.setText(" ");
+        boton0.setTextColor( Color.BLACK);
+        //fijamos el relleno (espacio dentro del View)
+        boton0.setPadding(20, 20, 20, 20);
+        //fijamos el tipo de la letra y el estilo
+        boton0.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+        boton0.setBackgroundColor(Color.GREEN);
+        //suministra parámetros al padre de la View
+        boton0.setLayoutParams(getLayoutParams());
+        boton0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(pulsado[id]==false){
+                    if(jueganX==true){
+                        puntuar(id);
+                        boton0.setText("X");
+                        jueganX= false;
+                        mandarMensaje("Juega el Jugador 2");
+                        if(partida.GanaJugador1()==true){
+                            mandarMensaje("Ha ganado el Jugador 1");
+                            bloquearBotones();
+                        }
+                    }else{
+                        puntuar(id);
+                        boton0.setText("O");
+                        jueganX= true;
+                        mandarMensaje("Juega el Jugador 1");
+                        if(partida.GanaJugador2()==true){
+                            mandarMensaje("Ha ganado el Jugador 2");
+                            bloquearBotones();
+                        }
+                    }
+                    pulsado[id]= true;
+                }
+            }
+        });
+    }
     private TableRow.LayoutParams getLayoutParams() {
-        //ancho (tan grande como el padre - rellleno) y alto (tan grande como su contenido interno + relleno)
         TableRow.LayoutParams params = new TableRow.LayoutParams(
                 TableRow.LayoutParams.MATCH_PARENT,
                 TableRow.LayoutParams.WRAP_CONTENT);
@@ -107,4 +179,17 @@ public class MainActivity extends AppCompatActivity {
             partida.MueveJugador2(pos);
         }
     }
+
+    private void mandarMensaje(String mensaje){
+        Toast toast1 =
+                Toast.makeText(getApplicationContext(),
+                        mensaje, Toast.LENGTH_LONG);
+        toast1.show();
+    }
+    private void bloquearBotones(){
+        for (int cont=0; cont< pulsado.length; cont++){
+            pulsado[cont]=true;
+        }
+    }
+
 }
